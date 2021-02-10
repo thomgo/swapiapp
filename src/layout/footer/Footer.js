d1aa11edcf72576ee4560491432103ac9f26ff44
Thomas <tg.59290@gmail.com>
2021-02-10 12:38:47 +0100

    add thanks to swapi and change site title

--- src/layout/footer/Footer.js
+++ src/layout/footer/Footer.js
@@ -2,6 +2,7 @@ function Footer() {
     return(
         <header className="bg-dark text-white p-5 text-center mt-0 mb-0">
             <p>A sample app to help the students understand React and the REST API</p>
+            <p>Sincere Thanks to the SWAPI team (https://swapi.dev/) for their awesome free and open API</p>
         </header>
     );
 }
